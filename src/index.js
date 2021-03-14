bc4c3a3253f41154caac04724829f04a6bc28636
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 00:42:49 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,47 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function toyCollection() {
+  return fetch('http://localhost:3000/toys')
+            .then(resp => resp.json())
+            .then(function(obj) {
+              const div = document.getElementById('toy-collection');
+              for (const toy of obj) {
+                const divCard = document.createElement('div');
+                const h2 = document.createElement('h2');
+                const img = document.createElement('img');
+                const p = document.createElement('p');
+                const button = document.createElement('button');
+                h2.innerText = toy.name;
+                img.src = toy.image;
+                p.innerText = toy.likes;
+                button.className = "like-btn";
+                divCard.appendChild(h2);
+                divCard.appendChild(img);
+                divCard.appendChild(p);
+                divCard.appendChild(button);
+                div.appendChild(divCard);
+              }
+            });
+}
+
+function addToys() {
+  let name = document.getElementsByClassName('input-text').name.value;
+  let image = document.getElementsByClassName('input-text').image.value
+  return fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "image": image,
+      "likes": 0
+    })
+  })
+    .then(resp => resp.json())
+    .then(obj => console.log(obj))
+    .catch(error => console.log(error.message));
+}
